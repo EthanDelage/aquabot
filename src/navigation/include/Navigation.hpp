0675b05199f72fa3950fb505d0165f61e0399d9d
tdameros <tomdamerose@gmail.com>
2023-10-15 15:23:01 +0200

    position and thruth work properly

--- src/navigation/include/Navigation.hpp
+++ src/navigation/include/Navigation.hpp
@@ -2,7 +2,13 @@
 #define NAVIGATION_HPP
 
 # include "rclcpp/rclcpp.hpp"
+# include "std_msgs/msg/float64.hpp"
 # include "ros_gz_interfaces/msg/param_vec.hpp"
+# include <cmath>
+
+# define THRUST_MAX 6000
+# define POS_MAX	M_PI_4
+# define POS_MIN	-POS_MAX
 
 class Navigation : public rclcpp::Node {
 
@@ -10,12 +16,17 @@ public:
 	Navigation();
 
 private:
-	rclcpp::Subscription<ros_gz_interfaces::msg::ParamVec>::SharedPtr	_pinger;
-
 	double	_bearing;
 	double	_range;
 
+	rclcpp::Subscription<ros_gz_interfaces::msg::ParamVec>::SharedPtr	_pinger;
+	rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr				_publisherThrust;
+	rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr				_publisherPos;
+
+
 	void	pingerCallback(ros_gz_interfaces::msg::ParamVec::SharedPtr msg);
+	void	setHeading(double bearing, double range);
+	double	regulator(double bearing);
 
 };
 
--- src/navigation/src/Navigation.cpp
+++ src/navigation/src/Navigation.cpp
@@ -1,11 +1,14 @@
 #include "Navigation.hpp"
 
+#include <algorithm>
+
 using std::placeholders::_1;
 
 Navigation::Navigation() : Node("navigation") {
 	_pinger = create_subscription<ros_gz_interfaces::msg::ParamVec>("/wamv/sensors/acoustics/receiver/range_bearing", 10,
 				std::bind(&Navigation::pingerCallback, this, _1));
-
+	_publisherThrust = create_publisher<std_msgs::msg::Float64>("/wamv/thrusters/main/thrust", 5);
+	_publisherPos = create_publisher<std_msgs::msg::Float64>("/wamv/thrusters/main/pos", 5);
 }
 
 void Navigation::pingerCallback(ros_gz_interfaces::msg::ParamVec::SharedPtr msg) {
@@ -17,4 +20,42 @@ void Navigation::pingerCallback(ros_gz_interfaces::msg::ParamVec::SharedPtr msg)
 		else if (name == "range")
 			_range = param.value.double_value;
 	}
+	setHeading(_bearing, _range);
+}
+
+void Navigation::setHeading(double bearing, double range) {
+	auto 	posMsg = std_msgs::msg::Float64();
+	auto 	thrustMsg = std_msgs::msg::Float64();
+	double	regulation;
+
+	regulation = regulator(bearing);
+	posMsg.data = (POS_MIN + regulation * 2 * POS_MAX);
+	thrustMsg.data = std::abs(std::abs(regulation - 0.5) - 0.5) * 2 * THRUST_MAX;
+	if (range < 20) {
+		posMsg.data = 0;
+		thrustMsg.data = 0;
+	}
+	std::cout << "Bearing: " << bearing << std::endl;
+	std::cout << "Range: " << range << std::endl;
+	std::cout << "Regulator: " << regulation << std::endl;
+	std::cout << "Pos: " << posMsg.data << std::endl;
+	std::cout << "Thrust: " << thrustMsg.data << std::endl << std::endl;
+	_publisherPos->publish(posMsg);
+	_publisherThrust->publish(thrustMsg);
+}
+
+double Navigation::regulator(double bearing) {
+	const double	goal = 0;
+	const double	k = 0.25;
+	double			gap;
+	double			percentage;
+
+	gap = bearing - goal;
+	gap = (gap + M_PI) / (2 * M_PI);
+	percentage = gap * k;
+	std::cout << "%: " << percentage << std::endl;
+	percentage = std::min(percentage, 1.);
+	percentage = std::max(percentage, 0.);
+	std::cout << "%: " << percentage << std::endl;
+	return (percentage);
 }
