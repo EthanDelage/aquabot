9043888e411b62a67e2f86b4cf0bf8ebdfa53b59
tdameros <tomdamerose@gmail.com>
2023-11-25 17:43:59 +0100

    refactor using types

--- src/perception/perception/perception.py
+++ src/perception/perception/perception.py
@@ -1,27 +1,27 @@
-import math
+from typing import Optional, Tuple
+import timeit
+import time
 
 import cv2
-import numpy as np
 import rclpy
 from rclpy.node import Node
-import cv2 as cv
 from cv_bridge import CvBridge
-from uuid import uuid4
-
+import numpy as np
+import matplotlib.pyplot as plt
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import CameraInfo
 from sensor_msgs.msg import PointCloud2
 
-import numpy as np
-import matplotlib.pyplot as plt
 from pprint import pprint
+from .camera import Camera
+from .lidar import Lidar
 
-import struct
+BoundingBox = Tuple[int, int, int, int, float]
 
 
 class Perception(Node):
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__('perception')
         self.image_subscription = self.create_subscription(
             Image,
@@ -40,243 +40,166 @@ class Perception(Node):
             self.lidar_callback,
             10
         )
-        self.bridge = CvBridge()
-        self.horizontal_fov = 1.3962634
-        self.image = None
-        self.camera = None
-        self.camera_translation_matrix = np.array([
-            [1, 0, 0, 0],
-            [0, 1, 0, 0],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1]
-        ])
-        self.camera_rotation_matrix = np.array([
-            [1, 0, 0, 0],
-            [0, np.cos(0.26), -np.sin(0.26), 0],
-            [0, np.sin(0.26), np.cos(0.26), 0],
-            [0, 0, 0, 1]
-        ])
-        self.camera_rotation_matrix1 = np.array([
-            [np.cos(np.pi / 2), 0, np.sin(np.pi / 2), 0],
-            [0, 1, 0, 0],
-            [-np.sin(np.pi / 2), 0, np.cos(np.pi / 2), 0],
-            [0, 0, 0, 1]
-        ])
-        self.camera_rotation_matrix = np.dot(self.camera_rotation_matrix, self.camera_rotation_matrix1)
-        self.camera_transformation_matrix = np.dot(self.camera_translation_matrix, self.camera_rotation_matrix)
-        # self.camera_transformation_matrix = self.camera_rotation_matrix
-        # self.camera_transformation_matrix = self.camera_translation_matrix
-        self.enemy_bearing = None
+        self.bridge: CvBridge = CvBridge()
+        self.image: Optional[np.ndarray] = None
+        self.enemy_bearing: Optional[float] = None
+        self.lidar: Lidar = Lidar()
+
+        self.camera: Optional[Camera] = None
+        self.display_plt_graph: bool = False
+        self.fig = None
+        self.ax = None
+        self.scatter_plot = None
+
+        self.rgb_lower_red: np.ndarray = np.array([65, 6, 5])
+        self.rgb_upper_red: np.ndarray = np.array([110, 16, 15])
+        self.rgb_lower_green: np.ndarray = np.array([7, 50, 0])
+        self.rgb_upper_green: np.ndarray = np.array([28, 81, 10])
 
     def image_callback(self, msg):
         self.image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
         self.detect_red_boat()
-        key = cv.waitKey(1)
-        if key == ord('q'):
-            cv.destroyAllWindow()
-            exit(0)
-        elif key == ord('o'):
-            cv.imwrite(
-                'src/perception/resource/positive/{}.jpg'.format(uuid4()),
-                self.image)
-        elif key == ord('x'):
-            cv.imwrite(
-                'src/perception/resource/negative/{}.jpg'.format(uuid4()),
-                self.image)
 
     def camera_callback(self, msg):
-        projection_matrix = msg.p
-        image_width = msg.width
-        fx = projection_matrix[0]
-        self.camera = msg
-        self.horizontal_fov = 2 * math.atan(image_width / (2 * fx))
-
-    def get_all_lidar_points(self, point_cloud_msg):
-        if point_cloud_msg.data:
-            # Assuming XYZ point cloud data with float32 encoding
-            # Get the point cloud data as a bytearray
-            data_buffer = bytearray(point_cloud_msg.data)
-
-            # Define variables for the point cloud data
-            points = []
-
-            # Calculate the offset and stride for XYZ (assuming x, y, z are the first 3 fields)
-            x_offset = point_cloud_msg.fields[0].offset
-            y_offset = point_cloud_msg.fields[1].offset
-            z_offset = point_cloud_msg.fields[2].offset
-            intensity_offset = point_cloud_msg.fields[3].offset
-            ring_offset = point_cloud_msg.fields[4].offset
-            point_step = point_cloud_msg.point_step
-
-            # Iterate through the data and extract XYZ coordinates
-            for i in range(0, len(data_buffer), point_step):
-                x = struct.unpack_from('f', data_buffer, i + x_offset)[0]
-                y = struct.unpack_from('f', data_buffer, i + y_offset)[0]
-                z = struct.unpack_from('f', data_buffer, i + z_offset)[0]
-                intensity = struct.unpack_from('f', data_buffer,
-                                               i + intensity_offset)[0]
-                ring = struct.unpack_from('H', data_buffer,
-                                          i + ring_offset)[0]
-                points.append([x, y, z])
-            # Convert the list of points to a NumPy array
-            parsed_points = np.array(points, dtype=np.float32)
-            return parsed_points
-        return None
-
-    def filter_visible_lidar_points(self, lidar_points, camera_info):
-        # Matrice de projection de la caméra P (à partir de vos données de la caméra)
-        P = camera_info.p
-        P = P.reshape(3, 4)
-        # print("after")
-        # pprint(P)
-        # exit(1)
-
-        visible = []
-        if self.image is None:
-            return np.array(visible)
-        for point_3d in lidar_points:
-            inf = np.isinf(point_3d)
-            if inf[0] or inf[1] or inf[2]:
-                continue
-            # print(f"3D: {point_3d}")
-            point_4d = np.array([point_3d[0], point_3d[1], point_3d[2], 1])
-            point_4d = np.array([point_4d[0], point_4d[2], point_4d[1], 1])
-            point_4d = np.dot(self.camera_transformation_matrix, point_4d)
-            point_4d /= point_4d[3]
-            # point_4d = np.array([point_4d[2], point_4d[1], point_4d[0], 1])
-            point_2d = np.dot(P, point_4d)
-            # print(f"4D: {point_4d}")
-            # print(f"2D: {point_2d}")
-            if point_2d[2] >= 0:
-                continue
-            x = point_2d[0] / point_2d[2]
-            y = point_2d[1] / point_2d[2]
-            is_visible = 0 < x < self.camera.width and 0 < y < self.camera.height
-            if is_visible:
-                x = int(x)
-                y = int(y)
-                # y = self.camera.height - y - 1
-                # x = self.camera.width - x - 1
-                # print(f"new x/y: {x} {y}")
-                b = self.image[y, x][0] / 255
-                g = self.image[y, x][1] / 255
-                r = self.image[y, x][2] / 255
-                self.image[y, x] = [255, 255, 255]
-                # b = self.image[x, y][0] / 255
-                # g = self.image[x, y][1] / 255
-                # r = self.image[x, y][2] / 255
-                # pprint(f"Color: {r, g, b}")
-                visible.append(np.array([point_3d[0], point_3d[1], point_3d[2], r, g, b]))
-        return np.array(visible)
+        if self.camera is None:
+            projection_matrix = msg.p.reshape(3, 4)
+            resolution = (msg.width, msg.height)
+            horizontal_fov = 1.3962634
+            self.camera = Camera(projection_matrix, horizontal_fov, resolution)
 
     def lidar_callback(self, point_cloud_msg):
+        # start = time.time()
+        timer = timeit.Timer(lambda: self.lidar.parse_points(point_cloud_msg))
+        time_exec = timer.timeit(number=1)
+        pprint(time_exec * 1000)
+        self.lidar.parse_points(point_cloud_msg)
+        if self.camera is not None and self.image is not None:
+            self.lidar.project_points_to_camera(self.camera, self.image)
+            # self.draw_lidar_points_in_image()
+            # self.draw_plt_graph()
+            self.update_plot()
+        # end = time.time()
+        # pprint((end - start) * 1000)
 
-        parsed_points = self.get_all_lidar_points(point_cloud_msg)
-        # parsed_points = np.array([[15, 10, 0]])
-        if self.camera != None:
-            cam_points = self.filter_visible_lidar_points(parsed_points, self.camera)
-            if len(cam_points) > 0:
-                parsed_points = cam_points
-        # pprint(f"Fields:\n{point_cloud_msg.fields}")
+    def detect_red_boat(self):
+        rgb_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+        rgb_red_mask = cv2.inRange(rgb_image, self.rgb_lower_red,
+                                   self.rgb_upper_red)
+        rgb_green_mask = cv2.inRange(rgb_image, self.rgb_lower_green,
+                                     self.rgb_upper_green)
+        red_contours, _ = cv2.findContours(rgb_red_mask, cv2.RETR_EXTERNAL,
+                                           cv2.CHAIN_APPROX_SIMPLE)
+        green_contours, _ = cv2.findContours(rgb_green_mask, cv2.RETR_EXTERNAL,
+                                             cv2.CHAIN_APPROX_SIMPLE)
+        largest_bounding_box: Optional[BoundingBox] = None
+        for contour in red_contours:
+            if self.is_enemy_contour(contour, green_contours):
+                x, y, w, h = cv2.boundingRect(contour)
+                size = w * h
+                largest_bounding_box = self.max_bounding_box(
+                    largest_bounding_box, (x, y, w, h, size))
+                cv2.rectangle(self.image, (x, y), (x + w, y + h), (0, 255, 0),
+                              1)
+        self.calculate_enemy_bearing(largest_bounding_box)
 
-        # SAVEEEEEEE
-        # Affichage des points dans un nuage 3D avec Matplotlib
-                cv2.imshow('Lidar Tracking', self.image)
-                # fig = plt.figure()
-                # ax = fig.add_subplot(111, projection='3d')
-                # # Réduire la taille des marqueurs pour afficher des points moins hauts
-                # ax.set_zlim([0, 50])  # Remplacez min_z_value et max_z_value par les valeurs Z minimales et maximales que vous souhaitez afficher
-                #
-                # # Séparation des coordonnées XYZ
-                # x = parsed_points[:, 0]
-                # y = parsed_points[:, 1]
-                # z = parsed_points[:, 2]
-                # r = parsed_points[:, 3]
-                # g = parsed_points[:, 4]
-                # b = parsed_points[:, 5]
-                # # Affichage des points
-                # colors = np.column_stack((r, g, b))
-                # ax.scatter(x, y, z, c=colors, cmap='viridis',
-                #            s=1)  # La couleur est basée sur l'axe Z
-                # # for i in range(len(parsed_points)):
-                # #     plt.scatter(x[i], y[i], z[i], c=np.array([r[i], g[i], b[i]]))
-                #
-                # # Réglages d'affichage
-                # ax.set_xlabel('X Label')
-                # ax.set_ylabel('Y Label')
-                # ax.set_zlabel('Z Label')
-                #
-                # plt.show()
+    @staticmethod
+    def is_enemy_contour(red_contour, green_contours):
+        for green_contour in green_contours:
+            if Perception.is_contour_inside_rect(red_contour, green_contour):
+                return False
+        return True
 
     @staticmethod
     def is_contour_inside_rect(contour1, contour2):
         rect1 = cv2.boundingRect(contour1)
         rect2 = cv2.boundingRect(contour2)
-
         x1, y1, w1, h1 = rect1
         x2, y2, w2, h2 = rect2
-
         # Check if rect1 (contour1) is inside rect2 (contour2)
-        if x2 <= x1 <= x1 + w1 <= x2 + w2 and y2 <= y1 <= y1 + h1 <= y2 + h2:
-            return True
-        else:
-            return False
+        return x2 <= x1 <= x1 + w1 <= x2 + w2 and y2 <= y1 <= y1 + h1 <= y2 + h2
 
-    def detect_red_boat(self):
-        rgb_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
-        rgb_lower_red = np.array([65, 6, 5])
-        rgb_upper_red = np.array([110, 16, 15])
-        rgb_lower_green = np.array([7, 50, 0])
-        rgb_upper_green = np.array([28, 81, 10])
-        rgb_red_mask = cv2.inRange(rgb_image, rgb_lower_red, rgb_upper_red)
-        rgb_green_mask = cv2.inRange(rgb_image, rgb_lower_green,
-                                     rgb_upper_green)
+    @staticmethod
+    def max_bounding_box(largest_bounding_box: BoundingBox,
+                         current_bounding_box: BoundingBox) -> BoundingBox:
+        if largest_bounding_box is None or current_bounding_box[4] > \
+                largest_bounding_box[4]:
+            return current_bounding_box
+        return largest_bounding_box
 
-        # Find contours in the binary mask
-        red_contours, _ = cv2.findContours(rgb_red_mask, cv2.RETR_EXTERNAL,
-                                           cv2.CHAIN_APPROX_SIMPLE)
-        green_contours, _ = cv2.findContours(rgb_green_mask, cv2.RETR_EXTERNAL,
-                                             cv2.CHAIN_APPROX_SIMPLE)
-
-        # Draw bounding boxes around detected red regions
-        largest_bounding_box = None
-        for contour in red_contours:
-            is_enemy = True
-            for green_contour in green_contours:
-                if self.is_contour_inside_rect(contour, green_contour):
-                    is_enemy = False
-                    break
-            if is_enemy:
-                x, y, w, h = cv2.boundingRect(contour)
-                size = w * h
-                if largest_bounding_box is None or size > largest_bounding_box[4]:
-                    largest_bounding_box = (x, y, w, h, size)
-                self.calculate_enemy_bearing(largest_bounding_box)
-                cv2.rectangle(self.image, (x, y), (x + w, y + h), (0, 255, 0),
-                              1)
-        # Display the result
-        # cv2.imshow('Enemy Tracking', self.image)
-
-    def calculate_enemy_bearing(self, largest_bounding_box):
+    def calculate_enemy_bearing(self, largest_bounding_box: BoundingBox) -> \
+            Optional[float]:
         if largest_bounding_box is not None and self.camera is not None:
             middle_x = largest_bounding_box[0] + (largest_bounding_box[2] / 2)
             dx = middle_x - (self.camera.width / 2)
-            self.enemy_bearing = (dx / self.camera.width) * self.horizontal_fov
+            self.enemy_bearing = (dx / self.camera.width) * \
+                                 self.camera.horizontal_fov
+            return self.enemy_bearing
 
+    def draw_lidar_points_in_image(self) -> None:
+        if self.lidar.visible_points is not None:
+            for point in self.lidar.visible_points:
+                rate = point.get_distance() % 50
+                if 0 <= point.get_distance() < 50:
+                    rgb = [100 + rate * 3, 0, 0]
+                elif 50 <= point.get_distance() < 100:
+                    rgb = [0, 100 + rate * 3, 0]
+                else:
+                    rgb = [0, 0, 100 + rate * 3]
+                self.image[
+                    point.image_position[1], point.image_position[0]] = rgb
+        cv2.imshow('Lidar Tracking', self.image)
+        key = cv2.waitKey(1)
+
+    def initialize_plot(self) -> None:
+        if not self.display_plt_graph:
+            self.fig = plt.figure()
+            self.ax = self.fig.add_subplot(111, projection='3d')
+            self.ax.set_xlabel('X Label')
+            self.ax.set_ylabel('Y Label')
+            self.ax.set_zlabel('Z Label')
+            self.display_plt_graph = True
+
+    def update_plot(self) -> None:
+        self.initialize_plot()
+        xs, ys, zs, rs, gs, bs = [], [], [], [], [], []
+        for point in self.lidar.visible_points:
+            x, y, z = point.position
+            color = point.color / 255
+            xs.append(x)
+            ys.append(y)
+            zs.append(z)
+            rs.append(color[2])
+            gs.append(color[1])
+            bs.append(color[0])
+        colors = np.column_stack((rs, gs, bs))
+        if self.scatter_plot is not None:
+            self.scatter_plot.remove()
+        self.scatter_plot = self.ax.scatter(xs, ys, zs, c=colors,
+                                            cmap='viridis', s=1)
+        plt.pause(0.01)
+
+    def draw_plt_graph(self) -> None:
+        self.initialize_plot()
+        if not self.display_plt_graph:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            points = np.array(
+                [point.position for point in self.lidar.visible_points])
+            colors = np.array([point.color[::-1] / 255 for point in
+                               self.lidar.visible_points])
+            ax.scatter(*points.T, c=colors, cmap='viridis', s=1)
+            ax.set_xlabel('X Label')
+            ax.set_ylabel('Y Label')
+            ax.set_zlabel('Z Label')
+            self.display_plt_graph = True
+            plt.show()
 
 
 def main(args=None):
-    print("start")
     rclpy.init(args=args)
-
-    percep = Perception()
-
-    rclpy.spin(percep)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    percep.destroy_node()
+    perception = Perception()
+    rclpy.spin(perception)
+    perception.destroy_node()
     rclpy.shutdown()
 
 
